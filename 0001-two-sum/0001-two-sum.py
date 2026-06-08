9226f753fc17a25cf899655e75eeef15bdebdb90
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-08 11:31:07 +0300

    Time: 9 ms (33.46%), Space: 21 MB (8.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        # Sorting + Two Pointer Solution
+        num_index_array = []
+        for i, num in enumerate(nums):
+            num_index_array.append([num, i])
         
+        num_index_array.sort()
+        
+        i, j = 0, len(nums)-1
+        while i < j:
+            result = num_index_array[i][0] + num_index_array[j][0]
+            if result < target:
+                i+=1
+            elif result > target:
+                j-=1
+            elif result == target: 
+                return [min(num_index_array[i][1], num_index_array[j][1]),
+                        max(num_index_array[i][1], num_index_array[j][1])]
+        return []
