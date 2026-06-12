4f005652f1b53d46ba0dbbe419fedfee1221babb
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-12 15:41:30 +0300

    Time: 1 ms (83.46%), Space: 19.3 MB (35.42%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        columns = defaultdict(set)
-        squares = defaultdict(set)
+        # bit mask solution (very fast and memory efficient)
+        # fast -> bit operations are done by native cpu assembly instructions in one clock cycle
+        # memory efficient: because hashs and sets require dynamic memory allocations
+
+        rows = [0] * 9
+        columns = [0] * 9
+        squares = [0] * 9
+
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if ( board[r][c] in rows[r] or 
-                    board[r][c] in columns[c] or
-                    board[r][c] in squares[r//3, c//3]):
+                
+                digit = int(board[r][c]) # 0-indexed bitmasking
+                digit_mask = 1 << digit
+                square_idx = (r // 3) * 3 + (c // 3)
+                if (digit_mask & rows[r]) or (digit_mask & columns[c]) or (digit_mask & squares[square_idx]):
                     return False
-                else:
-                    rows[r].add(board[r][c])
-                    columns[c].add(board[r][c])
-                    squares[r//3, c//3].add(board[r][c])
+                rows[r] |= digit_mask
+                columns[c] |= digit_mask
+                squares[square_idx] |= digit_mask
+
         return True
