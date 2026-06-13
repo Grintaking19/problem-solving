99b5b9b86f5f33d4c858760d3700039cb537d614
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-14 00:24:09 +0300

    Time: 27 ms (40.53%), Space: 21.2 MB (22.17%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -3,16 +3,19 @@ class Solution:
         result = [-1] * len(nums)
         stack = []
 
-        for i  in range(len(nums)):
-            while stack and nums[i] > stack[-1][0]:
-                popped_item = stack.pop()
-                result[popped_item[1]] = nums[i]
-            stack.append((nums[i],i))
 
-        while stack:
-            popped = stack.pop()
-            for i in range(popped[1]):
-                if nums[i] > popped[0]:
-                    result[popped[1]] = nums[i]
-                    break
+        for i  in range(2*len(nums)):
+            current_idx = i % len(nums)
+            
+            while stack and nums[current_idx] > nums[stack[-1]]:
+                popped_idx = stack.pop()
+                result[popped_idx] = nums[current_idx]
+            
+
+            if i < len(nums):
+                stack.append(current_idx)
+            
+            # early exit
+            if not stack:
+                break
         return result
