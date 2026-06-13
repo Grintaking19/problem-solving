2e231165c59aa9e94a4a2fd6653831b39fdc496d
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-13 23:10:03 +0300

    Time: 2 ms (43%), Space: 19.5 MB (28.5%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,16 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         greater_dict = {}
-        for i in range(len(nums2)):
-            for j in range(i+1, len(nums2)):
-                if nums2[j] > nums2[i]:
-                    greater_dict[nums2[i]] = nums2[j]
-                    break
-        print(greater_dict)
-        result = []
-        for num in nums1:
-            if num in greater_dict:
-                result.append(greater_dict[num])
-            else: 
-                result.append(-1)
-        return result
+        stack = [] # Monotonic Decreasing Stack
+
+        for num in nums2:
+            while stack and num > stack[-1]: # Pop and assign dictioanry
+                popped_item = stack.pop()
+                greater_dict[popped_item] = num
+
+            stack.append(num)
+
+        while stack:
+            popped_item = stack.pop()
+            greater_dict[popped_item] = -1
+        
+        return [greater_dict[x] for x in nums1]
