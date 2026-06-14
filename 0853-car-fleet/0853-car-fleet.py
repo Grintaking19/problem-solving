c133b7fc3df041847c145f17afa97cc9f4deea11
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-15 01:44:57 +0300

    Time: 167 ms (56.85%), Space: 41.1 MB (79.72%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = list(zip(position, speed))
+        stack = []
+        cars.sort(reverse=True)
+        for p, s in cars:
+            stack.append((target-p)/s)
+            while len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()        
+        return len(stack)
