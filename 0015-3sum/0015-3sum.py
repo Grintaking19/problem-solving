098f53501807b1ba5f28cfbe480042d734ccca4d
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-16 12:47:37 +0300

    Time: 439 ms (97.41%), Space: 22.1 MB (93.84%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,21 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-       nums.sort()
-       result =[]
-       for i in range(0,len(nums)-2):
-            l, r = i+1 ,len(nums) -1  
-            if i>0 and nums[i] == nums[i-1]:
+        nums.sort()
+        result = []
+
+        for i, a in enumerate(nums):
+            if a > 0: # Remaining elements are positive, No need to check them
+                break
+            if i > 0 and a == nums[i-1]: # skip duplicates
                 continue
-            while l<r:
-                if nums[l] +nums[r] < -nums[i]:
-                    l+=1
-                elif nums[l] +nums[r] > -nums[i]:
-                    r-=1
-                else :
-                    result.append([nums[i], nums[l] ,nums[r]])
+            
+            l = i+1
+            r = len(nums) - 1
+
+            while l < r:
+                threeSum = nums[l] + nums[r] + a
+                if threeSum > 0:
+                    r -=1
+                elif threeSum < 0:
+                    l +=1
+                else:
+                    result.append([nums[l], nums[r], a])
                     l += 1
                     r -= 1
-                    # Skip duplicates for the left pointer
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-       return result
+                    while nums[l] == nums[l - 1] and l < r:
+                        l +=1
+        return result
