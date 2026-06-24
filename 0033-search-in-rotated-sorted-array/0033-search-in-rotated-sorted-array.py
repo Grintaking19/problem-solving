7499c410164c86f0d8f68f0b29450c6b4adf5aaf
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-24 13:55:16 +0300

    Time: 0 ms (100%), Space: 19.4 MB (77.56%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # one pass binary search without finding pivot
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if target == nums[mid]:
+                return mid
+            
+            if nums[l] <= nums[mid]: # larger sorted elements, left-side
+                if target < nums[l] or target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            else:
+                if target > nums[r] or target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
