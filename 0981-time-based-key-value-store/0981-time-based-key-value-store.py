477d15af558585d55a3714cfd0ba21e17abaf9c1
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-24 14:59:36 +0300

    Time: 131 ms (53.92%), Space: 69 MB (53.08%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -3,23 +3,26 @@ from sortedcontainers import SortedDict
 class TimeMap:
 
     def __init__(self):
-        self.ds = defaultdict(SortedDict)
+        self.ds = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.ds[key][timestamp] = value
+        self.ds[key].append((value,timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.ds:
-            return ""
-
-        timestamps = self.ds[key]
-
-        idx = timestamps.bisect_right(timestamp) - 1
-
-        if idx >= 0:
-            time = timestamps.iloc[idx]
-            return self.ds[key][time]
-        return ""
+        array = self.ds[key]
+        l, r = 0, len(array)  - 1
+        res = ("", 0)
+        while l <= r:
+            mid = l + (r - l) // 2
+            if array[mid][1] == timestamp:
+                return array[mid][0]
+            elif array[mid][1] > timestamp:
+                r = mid - 1
+            else:
+                l = mid + 1
+                res = array[mid]
+                
+        return res[0]
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
